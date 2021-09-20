f958eb76a8bb787f3dd5218f8b47843a5ff8e570
Arash Chaychi Maleki <arashchm@gmail.com>
2021-09-20 18:42:38 +0300

    getting the project ready for development, added script inject to hubs code, added top level server to serve a list of script for each hub

---
+++ topLevelServer/config.js
@@ -0,0 +1,12 @@
+const config = {};
+
+config.hubsarray = [
+    {
+        hub_id: "nAm6WFM",
+        urls: [
+            'example'
+        ]
+    },
+];
+
+module.exports = config;
---
+++ topLevelServer/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+//load the config files which contains our hub_ids and their urls
+const config = require("./config");
+
+const app = express();
+app.use(cors());
+app.get(
+    "/injectScripts",
+    async (req, res) => {
+        const id = req.query.hubid;
+        const urls = config.hubsarray.find(x => x.hub_id === id)?.urls || [];
+        res.json(urls);
+    });
+
+
+app.listen(3000, () =>
+    console.log("HTTP Server running on port 3000")
+);
